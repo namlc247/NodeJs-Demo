a182515086c6b5917f95fda253936044445cc0ca
NamNguyen <namlc247@gmail.com>
2023-08-15 11:54:44 +0700

    favourite route

--- routes/api-favourite.js
+++ routes/api-favourite.js
@@ -2,7 +2,7 @@ const conn = require('../connect');
 
 module.exports = function (app) {
   app.get('/api/favourite', function (req, res) {
-    let sql = 'SELECT * FROM favourite ORDER BY id DESC';
+    let sql = 'SELECT * FROM favourite';
 
     conn.query(sql, function (err, result) {
       res.send({
@@ -13,51 +13,36 @@ module.exports = function (app) {
   });
 
   app.post('/api/favourite', function (req, res) {
-    let sql = 'INSERT INTO favourite SET ?';
+    let sqlCheck = 'SELECT * FROM favourite WHERE account_id = ? AND product_id = ?';
 
-    conn.query(sql, req.body, function (err, result) {
-      req.body.id = result.insertId;
-      res.send({
-        result: req.body,
-        status: 200,
-      });
+    conn.query(sqlCheck, [req.body.account_id, req.body.product_id], (err, data) => {
+      if (data.length > 0) {
+        let sqlDelete = 'DELETE FROM favourite WHERE account_id = ? AND product_id = ?';
+
+        conn.query(sqlDelete, [req.body.account_id, req.body.product_id], (err, result) => {
+          res.send({
+            result: 'Remove from favourite',
+            status: 200
+          })
+        })
+      } else {
+        let sqlInsert = 'INSERT INTO favourite SET ?';
+
+        conn.query(sqlInsert, req.body, (err, result) => {
+          if (!err) {
+            res.send({
+              result: 'Add to favourite',
+              status: 200
+            })
+          } else {
+            res.send({
+              result: null,
+              status: 500
+            })
+          }
+        })
+      }
     });
-  });
 
-  app.get('/api/favourite/:id', function (req, res) {
-    let id = req.params.id;
-    let sql = 'SELECT * FROM favourite WHERE id = ?';
-
-    conn.query(sql, [id], function (err, data) {
-      res.send({
-        result: data.length ? data[0] : '',
-        status: 200,
-      });
-    });
-  });
-
-  app.delete('/api/favourite/:id', function (req, res) {
-    let id = req.params.id;
-    let sql = 'DELETE FROM favourite WHERE id = ?';
-
-    conn.query(sql, [id], function (err, data) {
-      res.send({
-        result: data,
-        status: 200,
-      });
-    });
-  });
-
-  app.put('/api/favourite/:id', function (req, res) {
-    let id = req.params.id;
-    let sql = 'UPDATE favourite SET ? WHERE id = ?';
-
-    conn.query(sql, [req.body, id], function (err, result) {
-      req.body.id = id;
-      res.send({
-        result: req.body,
-        status: 200,
-      });
-    });
   });
 };
